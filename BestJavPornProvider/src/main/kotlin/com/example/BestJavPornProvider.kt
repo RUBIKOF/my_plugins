af34e7e2f3d4feab5c9463ca770acf8908550a29
RUBIKOF <ca_sim@hotmail.com>
2024-11-09 19:32:12 -0600

    Otro

---
+++ BestJavPornProvider/src/main/kotlin/com/example/BestJavPornProvider.kt
@@ -0,0 +1,232 @@
+package com.example
+
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.mvvm.logError
+import com.lagradost.cloudstream3.utils.AppUtils.parseJson
+import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
+import com.lagradost.cloudstream3.utils.getQualityFromName
+import com.lagradost.cloudstream3.utils.loadExtractor
+import java.util.*
+
+
+class BestJavPornProvider : MainAPI() {
+    companion object {
+        fun getType(t: String): TvType {
+            return if (t.contains("OVA") || t.contains("Especial")) TvType.OVA
+            else if (t.contains("Pelicula")) TvType.AnimeMovie
+            else TvType.Anime
+        }
+    }
+
+    override var mainUrl = "https://bestjavporn.me/"
+    override var name = "BestJavPorn"
+    override var lang = "es"
+    override val hasMainPage = true
+    override val hasChromecastSupport = true
+    override val hasDownloadSupport = true
+    override val supportedTypes = setOf(
+            TvType.NSFW
+    )
+    //https://bestjavporn.me/page/1?filter=latest
+    override val mainPage = mainPageOf(
+            "$mainUrl/page/" to "Main Page",
+    )
+    val saveImage = "";
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val urls = listOf(
+                Pair(
+                        "$mainUrl/familia",
+                        "Familia"
+                ),
+                Pair(
+                        "$mainUrl/milf",
+                        "Milf"
+                ),
+        )
+
+        val pagedLink = if (page > 0) "https://jav.guru/page/" + page + "?filter=latest" else "https://bestjavporn.me/?filter=latest"
+        val items = ArrayList<HomePageList>()
+        items.add(
+                HomePageList(
+                        "Recientes",
+                        app.get(pagedLink).document.select(".videos-list article").map {
+                            val title = it.selectFirst("img")?.attr("alt")
+                            val poster = it.selectFirst("img")?.attr("src")
+                            val dubstat = if (title!!.contains("Latino") || title.contains("Castellano"))
+                                DubStatus.Dubbed else DubStatus.Subbed
+                            //val poster = it.selectFirst("a div img")?.attr("src") ?: ""
+
+                            val url = it.selectFirst("a")?.attr("href") ?: ""
+
+
+                            newAnimeSearchResponse(title, url) {
+                                this.posterUrl = poster
+                                addDubStatus(dubstat)
+                            }
+                        })
+        )
+        urls.apmap { (url, name) ->
+            val soup = app.get(url).document
+            var texto: String
+            var inicio: Int
+            var ultimo: Int
+            var link: String
+            var z: Int
+            var poster = ""
+            val home = soup.select(".elementor-post__card").map {
+                val title = it.selectFirst(".elementor-post__title")?.text()
+                texto = it.selectFirst(".elementor-post__thumbnail img").toString()
+                inicio = texto.indexOf("data-lazy-srcset") + 18
+                ultimo = texto.length
+                link = texto.substring(inicio, ultimo).toString()
+                z = link.indexOf(" ")
+                poster = link.substring(0, z).toString()
+
+                AnimeSearchResponse(
+                        title!!,
+                        fixUrl(it.selectFirst("a")?.attr("href") ?: ""),
+                        this.name,
+                        TvType.Anime,
+                        fixUrl(poster),
+                        null,
+                        if (title.contains("Latino") || title.contains("Castellano")) EnumSet.of(
+                                DubStatus.Dubbed
+                        ) else EnumSet.of(DubStatus.Subbed),
+                )
+            }
+            items.add(HomePageList(name, home))
+        }
+
+        if (items.size <= 0) throw ErrorLoadingException()
+        return HomePageResponse(items, hasNext = true)
+
+    }
+
+    data class MainSearch(
+            @JsonProperty("animes") val animes: List<Animes>,
+            @JsonProperty("anime_types") val animeTypes: AnimeTypes
+    )
+
+    data class Animes(
+            @JsonProperty("id") val id: String,
+            @JsonProperty("slug") val slug: String,
+            @JsonProperty("title") val title: String,
+            @JsonProperty("image") val image: String,
+            @JsonProperty("synopsis") val synopsis: String,
+            @JsonProperty("type") val type: String,
+            @JsonProperty("status") val status: String,
+            @JsonProperty("thumbnail") val thumbnail: String
+    )
+
+    data class AnimeTypes(
+            @JsonProperty("TV") val TV: String,
+            @JsonProperty("OVA") val OVA: String,
+            @JsonProperty("Movie") val Movie: String,
+            @JsonProperty("Special") val Special: String,
+            @JsonProperty("ONA") val ONA: String,
+            @JsonProperty("Music") val Music: String
+    )
+
+    override suspend fun search(query: String): List<SearchResponse> {
+
+        val soup = app.get("$mainUrl//?s=$query").document
+        var texto: String
+        var inicio: Int
+        var ultimo: Int
+        var link: String
+        var z: Int
+        var poster = ""
+
+            return app.get("$mainUrl//?s=$query").document
+                    .select(".elementor-posts-container").select(".elementor-post__card").mapNotNull {
+                        texto = it.selectFirst(".elementor-post__thumbnail img").toString()
+                        inicio = texto.indexOf("srcset=") + 7
+                        ultimo = texto.length
+                        link = texto.substring(inicio, ultimo).toString()
+                        z = link.indexOf(" ")
+                        val image = link.substring(0, z).replace("\"","")
+                        val title = it.selectFirst(".elementor-post__title > a")?.text().toString()
+                        val url = fixUrlNull(it.selectFirst("a")?.attr("href") ?: "") ?: return@mapNotNull null
+
+
+                        MovieSearchResponse(
+                                title,
+                                url,
+                                this.name,
+                                TvType.NSFW,
+                                image
+                        )
+        }
+
+    }
+    data class EpsInfo (
+            @JsonProperty("number" ) var number : String? = null,
+            @JsonProperty("title"  ) var title  : String? = null,
+            @JsonProperty("image"  ) var image  : String? = null
+    )
+    override suspend fun load(url: String): LoadResponse {
+        val doc = app.get(url, timeout = 120).document
+        //val poster = "https://javenspanish.com/wp-content/uploads/2022/01/JUFE-132.jpg"
+        val title = doc.selectFirst(".inside-article h1")?.text()?:""
+        val type = "NFSW"
+        val description = doc.selectFirst("#content > div > div > div > div > section > div > div > div > div > div > div.elementor-element.elementor-element-9da66e1.elementor-widget.elementor-widget-text-editor > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > div > span")?.text()
+
+        val poster = doc.selectFirst(".inside-article img")?.attr("src")
+        //Fin espacio prueba
+        return MovieLoadResponse(
+                name = title,
+                url = url,
+                apiName = this.name,
+                type = TvType.NSFW,
+                dataUrl = url,
+                posterUrl = poster
+        )
+
+    }
+
+   /* override suspend fun loadLinks(
+            data: String,
+            isCasting: Boolean,
+            subtitleCallback: (SubtitleFile) -> Unit,
+            callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        try{
+            val url = "https://ds2play.com/e/8vxqazdt0aej"
+            loadExtractor(
+                    url = url,
+                    subtitleCallback = subtitleCallback,
+                    callback = callback
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            logError(e)
+        }
+        return false
+    }*/
+   override suspend fun loadLinks(
+           data: String,
+           isCasting: Boolean,
+           subtitleCallback: (SubtitleFile) -> Unit,
+           callback: (ExtractorLink) -> Unit
+   ): Boolean {
+       val f = listOf("https://streamtape.net/e/4zv4vA4y9rI284/","https://streamtape.com/e/4zv4vA4y9rI284/","https://ds2play.com/e/gli2qcwpmtvl")
+       f.mapNotNull{videos ->
+           fetchUrls(videos).map {
+               it.replace("https://dooood.com", "https://dood.ws")
+                       .replace("https://dood.sh", "https://dood.ws")
+                       .replace("https://dood.la","https://dood.ws")
+                       .replace("https://ds2play.com","https://dood.ws")
+                       .replace("https://dood.to","https://dood.ws")
+           }.apmap {
+               loadExtractor(it, data, subtitleCallback, callback)
+           }
+       }
+
+       return true
+   }
+}
