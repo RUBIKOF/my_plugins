a3383a04834a0822a93f586ec669fa88e1057e68
RUBIKOF <ca_sim@hotmail.com>
2024-11-14 14:18:51 -0600

    JavHD test

--- JavHDProvider/src/main/kotlin/com/example/JavHDProvider.kt
+++ JavHDProvider/src/main/kotlin/com/example/JavHDProvider.kt
@@ -80,7 +80,7 @@ class JavHDProvider : MainAPI() {
             if (url.contains("mother")) {
                 pagedLink = if (page > 1) "$mainUrl/mother/recent/" + page else "$mainUrl/mother/"
             } else if (url.contains("popular")) {
-                pagedLink = if (page > 1) "$mainUrl/popular/recent/" + page else "$mainUrl/popular/"
+                pagedLink = if (page > 1) "$mainUrl/popular/" + page else "$mainUrl/popular/"
             }
             val soup = app.get(pagedLink).document
 
