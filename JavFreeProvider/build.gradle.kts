007282eac0768a5257cc03853beae2a079a403a1
RUBIKOF <ca_sim@hotmail.com>
2025-01-27 16:42:14 -0600

    repair test

--- JavFreeProvider/build.gradle.kts
+++ JavFreeProvider/build.gradle.kts
@@ -1,6 +1,4 @@
-// use an integer for version numbers
 version = 8
-
 
 cloudstream {
     // All of these properties are optional, you can safely remove them
@@ -18,11 +16,42 @@ cloudstream {
     status = 1 // will be 3 if unspecified
 
     // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
+    // You can find a list of available types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
     tvTypes = listOf("NSFW")
 
     iconUrl = "https://www.google.com/s2/favicons?domain=javfree.sh&sz=%size%"
 
     language = "en"
+}
+
+android {
+    namespace = "com.example" // Reemplaza esto con tu namespace real
+
+    compileSdkVersion(30)
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 30
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8" // Required
+            // Disables some unnecessary features
+            freeCompilerArgs = freeCompilerArgs +
+                    "-Xno-call-assertions" +
+                    "-Xno-param-assertions" +
+                    "-Xno-receiver-assertions"
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.compilerArgs.add("-Xskip-metadata-version-check")
+    }
 }
